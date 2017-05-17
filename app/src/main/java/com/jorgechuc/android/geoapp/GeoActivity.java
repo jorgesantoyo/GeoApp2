26d75fdf91e133545b7b419dd01d5b3eede4e776
jorge luis santoyo uc <12470511@itcampeche.edu.mx>
2017-05-17 20:21:24 +0200

    version con demostracion de manejo de eventos del proyecto Geoapp

--- app/src/main/java/com/jorgechuc/android/geoapp/GeoActivity.java
+++ app/src/main/java/com/jorgechuc/android/geoapp/GeoActivity.java
@@ -2,12 +2,40 @@ package com.jorgechuc.android.geoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class GeoActivity extends AppCompatActivity {
+Button mbotoncierto:
+    Button mbotonfalso;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+
+
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geo);
+
+
+        mbotoncierto = (Button) findViewById(R.id.boton_cierto);
+        mbotonfalso = (Button) findViewById(R.id.boton_falso);
+
+mbotoncierto.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(GeoActivity.this, R.string.texto_correcto, Toast.LENGTH_SHORT).show();
+
+
+    }
+});
+mbotonfalso.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(GeoActivity.this, R.string.texto_incorrecto, Toast.LENGTH_SHORT).show();
+    }
+});
     }
 }
